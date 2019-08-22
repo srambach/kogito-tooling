0be37e2e156643fdc6eeec5120bfecc2cf605624
Joachim Schuler <jschuler@redhat.com>
2019-08-22 15:19:17 -0400

    add dark mode support

--- packages/vscode-extension-pack-simple-react/src/webview/index.ts
+++ packages/vscode-extension-pack-simple-react/src/webview/index.ts
@@ -16,6 +16,7 @@
 
 import "@patternfly/react-core/dist/styles/base.css";
 import "@patternfly/patternfly/patternfly-addons.scss";
+import "../styles/dark.scss";
 import * as MicroEditorEnvelope from "appformer-js-microeditor-envelope";
 import { SimpleReactEditorsFactory } from "./SimpleReactEditorsFactory";
 
