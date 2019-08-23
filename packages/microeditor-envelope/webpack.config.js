db6dea3b72519daa276099d950852da985da3efd
Joachim Schuler <jschuler@redhat.com>
2019-08-23 09:19:33 -0400

    use a common webpack config

--- packages/microeditor-envelope/webpack.config.js
+++ packages/microeditor-envelope/webpack.config.js
@@ -16,9 +16,10 @@
 
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
-const CircularDependencyPlugin = require("circular-dependency-plugin");
+const merge = require("webpack-merge");
+const baseConfig = require("../../base.webpack.config");
 
-module.exports = {
+module.exports = merge(baseConfig(__dirname), {
   mode: "development",
   devtool: "inline-source-map",
   entry: {
@@ -29,86 +30,5 @@ module.exports = {
     filename: "[name].js",
     libraryTarget: "commonjs2"
   },
-  externals: [nodeExternals({ modulesDir: "../../node_modules" })],
-  plugins: [
-    new CircularDependencyPlugin({
-      exclude: /node_modules/, // exclude detection of files based on a RegExp
-      failOnError: false, // add errors to webpack instead of warnings
-      cwd: process.cwd() // set the current working directory for displaying module paths
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        include: path.resolve(__dirname, 'src'),
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: false,
-              experimentalWatchApi: true,
-              configFile: path.resolve("./tsconfig.json")
-            }
-          }
-        ]
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader"
-        ],
-      },
-      {
-        test: /\.css$/,
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, '../../node_modules/patternfly'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/patternfly'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-core/dist/styles/base.css'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css')
-        ],
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(svg|ttf|eot|woff|woff2)$/,
-        include: [
-          path.resolve(__dirname, '../../node_modules/patternfly/dist/fonts'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-core/dist/styles/assets/fonts'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-core/dist/styles/assets/pficon'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/patternfly/assets/fonts'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/patternfly/assets/pficon')
-        ],
-        use: ['file-loader']
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif)$/i,
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, '../../node_modules/patternfly'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/patternfly/assets'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-core/dist/styles/assets/images'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-styles/css/assets/images'),
-          path.resolve(__dirname, '../../node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css/assets/images')
-        ],
-        use: ['file-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".jsx"],
-    modules: [path.resolve("../../node_modules"), path.resolve("./node_modules"), path.resolve("./src")]
-  }
-};
+  externals: [nodeExternals({ modulesDir: "../../node_modules" })]
+});
